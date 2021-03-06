7ab6b5f446d6a374a1eeeeb031cdb16783c698ca
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-03-06 21:56:15 +0900

    [Finish] 2-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -30,6 +30,6 @@ class TodolistsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = "f3a7167911f45310935293f8a50841967cfdfb1ca1fd889dd8a38c8af35048643b5cf5f9a3294e0b885da1c5be7afb8366c29f5f6085624c807f0501e93b474f"
